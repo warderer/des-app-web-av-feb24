6d6e1117131f6e0473202067f75ef09f99eed17e
César Guerra <warderer@gmail.com>
2024-04-16 12:39:44 -0600

    ✨ ToDoList Base

--- 02-todo-list/src/components/ToDoList.jsx
+++ 02-todo-list/src/components/ToDoList.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+
 const ToDoList = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleAdd = () => {
+    console.log('Agregando tarea:', inputValue)
+  }
+
   return (
-    <div>ToDoList</div>
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+
+      <button onClick={handleAdd}>Agregar</button>
+      <ul>
+        <li>tarea 1<button>Eliminar</button></li>
+        <li>tarea 2<button>Eliminar</button></li>
+        <li>tarea 3<button>Eliminar</button></li>
+      </ul>
+    </div>
   )
 }
 export default ToDoList
